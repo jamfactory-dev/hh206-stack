f37233a0908ac3b0e7c2080a653bcbea8d1377a5
Alec Siu <alecsiu@gmail.com>
2019-07-08 21:36:14 -0700

    Add h3

--- src/add-place/index.js
+++ src/add-place/index.js
@@ -1,21 +1,26 @@
 const AWS = require('aws-sdk');
 const uuidv5 = require('uuid/v5');
+const h3 = require("h3-js");
 
+const DEFAULT_RESOLUTION = 7;
 
 const generate_place_id = name => {
   return uuidv5(name, '2e59b050-a131-11e9-a43f-69a78e05c999');
 }
 
-
 exports.handler = async (event, context) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
   const place = event;
+  let item = {
+    place_id: generate_place_id(place.shortcut),
+    ...place
+  };
+  if (!!place.lat && !!place.lng) {
+    item.h3_index = h3.geoToH3(place.lat, place.lng, DEFAULT_RESOLUTION);
+  }
   const params = {
     TableName: process.env.PLACES_TABLE_NAME,
-    Item: {
-      place_id: generate_place_id(place.shortcut),
-      ...place
-    },
+    Item: item,
     ConditionExpression: 'attribute_not_exists(place_id)',
     ReturnConsumedCapacity: 'TOTAL'
   };
